948d3b5923f3506f118891394a96e41340c11e5b
Kevin Piette <kevin@termina.org>
2019-11-23 22:23:24 +0100

    Added autocompletion of paths

--- InteractiveConsole/InteractiveConsole/InteractiveConsole.cs
+++ InteractiveConsole/InteractiveConsole/InteractiveConsole.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace InteractiveConsole
 {
+    public class InputToken
+    {
+        public string Token { get; private set; }
+        public int StartIndex { get; private set; }
+        public bool IsValid => !string.IsNullOrEmpty(Token);
+        public int Length => Token.Length;
+
+        public InputToken(string token, int startIndex)
+        {
+            Token = token;
+            StartIndex = startIndex;
+        }
+    }
+
     public class InteractiveConsole
     {
+        private static int MAX_SUGGESTIONS = 16;
+
         private string _promptName;
         private int _promptLength;
 
@@ -16,6 +36,9 @@ namespace InteractiveConsole
         private bool _validate;
         private bool _setCursor;
 
+        private EnumerationOptions _enumerationOptions = new EnumerationOptions() { MatchCasing = MatchCasing.CaseInsensitive };
+        private string _currentDirectory = Directory.GetCurrentDirectory();
+
         public InteractiveConsole(string promptName)
         {
             _promptName = promptName;
@@ -25,6 +48,9 @@ namespace InteractiveConsole
         public void Process()
         {
             ConsoleKeyInfo key;
+            char lastKey = '\0';
+            DateTime lastTimeKeyPress = DateTime.MinValue;
+            bool doubleKeyPress;
 
             _input = string.Empty;
             _cursorPosition = 0;
@@ -34,6 +60,9 @@ namespace InteractiveConsole
             do
             {
                 key = Console.ReadKey(true);
+                doubleKeyPress = lastKey == key.KeyChar && (DateTime.UtcNow - lastTimeKeyPress).TotalMilliseconds < 500;
+                lastTimeKeyPress = DateTime.UtcNow;
+
                 _validate = false;
                 _print = false;
                 _setCursor = false;
@@ -44,7 +73,10 @@ namespace InteractiveConsole
                         AddChar(key.KeyChar);
                         break;
                     case ConsoleKey.Tab:
-                        AutoComplete();
+                        if (doubleKeyPress)
+                        {
+                            AutoComplete();
+                        }
                         break;
                     case ConsoleKey.Enter:
                         Validate();
@@ -65,6 +97,8 @@ namespace InteractiveConsole
                         AddChar(key.KeyChar);
                         break;
                 }
+
+                lastKey = key.KeyChar;
 
                 SetCursorVisible(false);
 
@@ -145,13 +179,121 @@ namespace InteractiveConsole
             _setCursor = true;
         }
 
-        private void AutoComplete()
+        private InputToken GetCurrentToken()
         {
-            string str = " insert ";
-            _input = _input.Insert(_cursorPosition, str);
-            _cursorPosition += str.Length;
+            if (_cursorPosition == 0)
+                return null;
+
+            var sb = new StringBuilder();
+
+            int i = _cursorPosition - 1;
+            for (; i >= 0; i--)
+            {
+                if(_input[i] == ' ')
+                {
+                    break;
+                }
+
+                sb.Insert(0, _input[i]);
+            }
+
+            return new InputToken(sb.ToString(), i + 1);
+        }
+
+        private List<string> GetPathSuggestions(string dir, string token)
+        {
+            int i = 0;
+            var suggestions = new List<string>();
+
+            foreach(string file in Directory.EnumerateFileSystemEntries(dir, $"{token}*", _enumerationOptions))
+            {
+                suggestions.Add(Path.GetFileName(file));
+                
+                if (++i >= MAX_SUGGESTIONS)
+                {
+                    break;
+                }
+            }
+
+            return suggestions;
+        }
+
+        private string GetCommonFromSuggestions(List<string> suggestions)
+        {
+            char c;
+            int i;
+            int u = 0;
+            string res = string.Empty;
+
+            while (true)
+            {
+                if (u >= suggestions[0].Length)
+                {
+                    return res;
+                }
+
+                c = suggestions[0][u];
+
+                for (i = 1; i < suggestions.Count; i++)
+                {
+                    if(u >= suggestions[i].Length || c != suggestions[i][u])
+                    {
+                        return res;
+                    }
+                }
+
+                u++;
+                res += c;
+            }
+        }
+
+        private void ReplaceToken(InputToken token, string oldValue, string newValue)
+        {
+            int index = token.StartIndex + token.Token.LastIndexOf(oldValue);
+            int count = token.StartIndex + token.Length - index;
+
+            _input = _input.Remove(index, count);
+            _cursorPosition -= count;
+
+            _input = _input.Insert(_cursorPosition, newValue);
+            _cursorPosition += newValue.Length;
+
             _print = true;
             _setCursor = true;
+        }
+
+        private void AutoComplete()
+        {
+            var inputToken = GetCurrentToken();
+
+            if(inputToken == null || !inputToken.IsValid)
+            {
+                return;
+            }
+
+            string dir = Path.Combine(_currentDirectory, new FileInfo(inputToken.Token).DirectoryName);
+            string token = Path.GetFileName(inputToken.Token);
+
+            var suggestions = GetPathSuggestions(dir, token);
+            if (suggestions.Count == 0)
+            {
+                return;
+            }
+
+            if (suggestions.Count == 1)
+            {
+                ReplaceToken(inputToken, token, suggestions[0]);
+                return;
+            }
+
+            string common = GetCommonFromSuggestions(suggestions);
+            if(common != token)
+            {
+                ReplaceToken(inputToken, token, common);
+                return;
+            }
+
+            Debug.WriteLine(string.Join(", ", suggestions));
         }
 
         private void Validate()
